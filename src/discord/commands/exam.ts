8fe2ec65c96391796ab508ec4df58bf99b0084f2
esta-rossa <rajiabdou9@gmail.com>
2021-10-25 15:58:31 +0100

    feat: add campus for `exam` command

--- src/discord/commands/exam.ts
+++ src/discord/commands/exam.ts
@@ -30,13 +30,27 @@ const examEmbed = (response: any[]): MessageEmbed => {
 const command: Command = {
   name: "exam",
   description: "Check if there is an exam currently",
+  options: [
+    {
+      type: "STRING",
+      name: "campus",
+      required: true,
+      description: "Your campus",
+      choices: [
+        { name: "Khouribga", value: "16" },
+        { name: "Benguerir", value: "21" },
+      ],
+    },
+  ],
   async execute(interaction) {
     await interaction.deferReply({
       ephemeral: process.env.EPHEMERAL === "true",
     });
 
+    const campus = interaction.options.getString("campus", true);
+
     const response = await got<Array<any>>(
-      "https://api.intra.42.fr/v2/campus/16/cursus/21/exams",
+      `https://api.intra.42.fr/v2/campus/${campus}/cursus/21/exams`,
       {
         headers: {
           Authorization: `Bearer ${process.env.INTRA_TOKEN}`,
@@ -51,7 +65,9 @@ const command: Command = {
     const now = Date.now();
     if (examEndTime.getTime() < now) {
       interaction.editReply(
-        `**NO** there is no exam currently. Last exam was ${examEndTime.toDateString()}`,
+        `**NO** there is no exam currently in ${
+          campus === "16" ? "Khouribga" : "Benguerir"
+        }. Last exam was ${examEndTime.toDateString()}`,
       );
     } else {
       interaction.editReply({
